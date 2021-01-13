4660e54482b03dfa786aadf22273de468aafe0ae
mahmoud Diab <mhmoddiab2@gmail.com>
2021-01-13 17:55:47 +0200

    mahmod

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const form = document.querySelector("form");
-      const output = document.querySelector("output");
-      const imgIcon = document.getElementById("imgIcon");
+const output = document.querySelector("output");
+const imgIcon = document.getElementById("imgIcon");
 
-      form.addEventListener("submit", (event) => {
-        // stop the form submitting and reloading the page
-        event.preventDefault();    
+form.addEventListener("submit", (event) => {
+  // stop the form submitting and reloading the page
+  event.preventDefault();
 
-        // get the value of the field with name="city"
-        const name = document.querySelector("#city").value
-        fetchWeather(name)
-    })
+  // get the value of the field with name="city"
+  const name = document.querySelector("#city").value;
+  fetchWeather(name);
+});
--- weather.js
+++ weather.js
@@ -1,39 +1,36 @@
 function fetchWeather() {
-    const form = document.querySelector("form");
-    const output = document.querySelector("output");
-    const imgIcon = document.getElementById("imgIcon");
-    const temp_f = document.getElementById("temp_f");
-    
-    
-    form.addEventListener("submit", (event) => {
-      // stop the form submitting and reloading the page
-      event.preventDefault();
-      // get the value of the field with name="city" from the form section
-  
-      const name = document.querySelector("#city").value
-      
-      fetch(
-        `http://api.weatherapi.com/v1/current.json?key=9865d5a7fd0645d788c152012211201&q=${name}`)
-      
-        .then((response) => { 
-          return response.json();
-        })
-        .then((weatherData) => {
-            
-            if (weatherData.error){
-                output.textContent =weatherData.error.message;
-            }
-         else  {output.textContent = weatherData.current.temp_c +" C ";
-              imgIcon.src = weatherData.current.condition.icon;
-              temp_f.textContent = weatherData.current.temp_f + " F";
-            fetchFlag(weatherData.location.country)
-            }   
-              
-        })
-        .catch((error) => {
-            output.textContent = ":warning: Something went wrong";
-            console.log(error)
-        });
+  const form = document.querySelector("form");
+  const output = document.querySelector("output");
+  const imgIcon = document.getElementById("imgIcon");
+  const temp_f = document.getElementById("temp_f");
+
+  form.addEventListener("submit", (event) => {
+    // stop the form submitting and reloading the page
+    event.preventDefault();
+    // get the value of the field with name="city" from the form section
+    output.innerHTML = "";
+
+    const name = document.querySelector("#city").value;
+
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=9865d5a7fd0645d788c152012211201&q=${name}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((weatherData) => {
+        if (weatherData.error) {
+          output.textContent = weatherData.error.message;
+        } else {
+          output.textContent = weatherData.current.temp_c + " C ";
+          imgIcon.src = weatherData.current.condition.icon;
+          temp_f.textContent = weatherData.current.temp_f + " F";
+          fetchFlag(weatherData.location.country);
+        }
+      })
+      .catch((error) => {
+        output.textContent = ":warning: Something went wrong";
+        console.log(error);
       });
-  
+  });
 }

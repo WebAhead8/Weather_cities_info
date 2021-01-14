1e177093aafe175e0d9fcb1dd45bcac89a7836c4
mahmoud Diab <mhmoddiab2@gmail.com>
2021-01-14 09:18:44 +0200

    add (S) to fetch weather

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ function fetchWeather(name) {
   const imgIcon = document.getElementById("imgIcon");
 
   fetch(
-    `http://api.weatherapi.com/v1/current.json?key=9865d5a7fd0645d788c152012211201&q=${name}`
+    `https://api.weatherapi.com/v1/current.json?key=9865d5a7fd0645d788c152012211201&q=${name}`
   )
     .then((response) => {
       return response.json();
